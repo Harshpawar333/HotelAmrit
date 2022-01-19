46c0c6b0b0ad5dbe6c840d2874e0422a826e35f8
Vishal Shinde <vishalshinde@Vishals-MacBook-Pro.local>
2022-01-19 15:28:44 +0530

    Added initial setup for hotel application

---
+++ src/components/FeaturedRooms.jsx
@@ -0,0 +1,7 @@
+import React, { Component } from "react";
+import Title from "./Title";
+export default class FeaturedRooms extends Component {
+  render() {
+    return <p>featured rooms section</p>;
+  }
+}
