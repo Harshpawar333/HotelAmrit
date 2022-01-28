2474bc5fd8435bd0656f32e48ae22e5adff7c59e
Vishal Shinde <vsvshinde15@gmail.com>
2022-01-28 16:30:13 +0530

    update public directory path

--- server/server.js
+++ server/server.js
@@ -27,7 +27,7 @@ app.use(express.static(path.join(__dirname, "client/build")));
 app.use("/api/hotel", hotelRoutes);
 
 app.get("*", (req, res) => {
-  res.sendFile(path.join(__dirname, "/client/build/index.html"));
+  res.sendFile(path.join(__dirname, "/client/build/public/index.html"));
 });
 
 app.listen(PORT, () => {
